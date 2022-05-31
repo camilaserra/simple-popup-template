f66d4886c914d0eadba6735c3d56c7906968aad4
Camila Serra <camilaserrasilva@gmail.com>
2022-05-31 09:07:38 -0300

    ✨ NEW FEATURE: Popup implemented

---
+++ popupScript.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.popup-container');
+const save = document.querySelector('.popup-save');
+
+save.addEventListener('click', ()=> {
+    container.style.display = 'none';
+});
